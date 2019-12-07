e21b1cf0b1bf7bfe8fc99c86bea2236fab0926ab
7325156 <57527406+7325156@users.noreply.github.com>
2019-12-07 16:40:56 +0800

    Update main.py
    导入原文中标题和作话顺序

--- main.py
+++ main.py
@@ -21,33 +21,55 @@ def get_sin(i,headers,fo,chinf):
     cont=requests.get(i,headers=headers).content
     dot=etree.HTML(cont.decode("GB18030","ignore").encode("utf-8","ignore").decode('utf-8'))
         
-        #获取标题及正文
+    #正文
     tex=dot.xpath("//html/body/table[@id='oneboolt']/tr[2]/td[1]/div[@class='noveltext']/text()")
+    #单章标题
     he=dot.xpath("string(//html/body/table[@id='oneboolt']/tr[2]/td[1]/div[@class='noveltext']/div[2]/h2)")
-        #获取作话
+    
+    #获取作话
     tex1=dot.xpath("//html/body/table[@id='oneboolt']/tr[2]/td[1]/div[@class='noveltext']/div[@class='readsmall']/text()")
-
-    tl="#"+str(tit[2])+"、"
-    tll=he
+    sign=dot.xpath("//*[@id='oneboolt']/tr[2]/td[1]/div/div[4]/@class") #用以确认正文和作话顺序
+    
+    #写入标题和内容提要
+    tl=str(tit[2])+"#"+" "
+    tll=he[0]
     if he=="":
         tll="$"
     tl=tl+tll
-
-        #写入文件
     fo.write(tl.rstrip())
     fo.write("  "+chinf[int(tit[2])].strip()+"\r\n")
-    for tn in tex:
-        vv=re.sub('@无限好文，尽在晋江文学城','',str(tn))
-        v=re.sub(' +', ' ', vv).rstrip()+"\r\n"
-        if v == "\r\n":
-            v=""
-        fo.write(v)
-    for m in tex1:
-        vv=re.sub('@无限好文，尽在晋江文学城','',str(m))
-        v=re.sub(' +', ' ', vv).rstrip()+"\r\n"
-        if v == "\r\n":
-            v=""
-        fo.write(v)
+    
+    #写入正文和作话
+    if str(sign) == "['readsmall']":
+        for m in tex1:
+            vv=re.sub('@无限好文，尽在晋江文学城','',str(m))
+            v=re.sub(' +', ' ', vv).rstrip()+"\r\n"
+            if v == "\r\n":
+                v=""
+            fo.write(v)
+        fo.write("----------\r\n")
+        for tn in tex:
+            vv=re.sub('@无限好文，尽在晋江文学城','',str(tn))
+            v=re.sub(' +', ' ', vv).rstrip()+"\r\n"
+            if v == "\r\n":
+                v=""
+            fo.write(v)
+    else:
+        for tn in tex:
+            vv=re.sub('@无限好文，尽在晋江文学城','',str(tn))
+            v=re.sub(' +', ' ', vv).rstrip()+"\r\n"
+            if v == "\r\n":
+                v=""
+            fo.write(v)
+        if len(tex1)!=0:
+            fo.write("----------\r\n")
+        for m in tex1:
+            vv=re.sub('@无限好文，尽在晋江文学城','',str(m))
+            v=re.sub(' +', ' ', vv).rstrip()+"\r\n"
+            if v == "\r\n":
+                v=""
+            fo.write(v)
+            
     print("第"+str(tit[2])+"章下载完成")
 
 #下载全部
@@ -63,7 +85,7 @@ def get_txt(txt_id):
     intro=ress.xpath("//html/body/table/tr/td[1]/div[2]/div[@id='novelintro']//text()")
     info=ress.xpath("string(//html/body/table/tr/td[1]/div[3])")
     
-    #获取标题
+    #获取文章标题
     titlem=ress.xpath("//html/head/title/text()")
     print("编号："+ ids + " 小说信息："+ str(titlem[0]) +" 开始下载。\r\n")
 
@@ -73,10 +95,11 @@ def get_txt(txt_id):
     href_list=ress.xpath("//html/body/table[@id='oneboolt']//tr/td[2]/span/div[1]/a/@href")
     #vip
     hhr=ress.xpath("//html/body/table[@id='oneboolt']//tr/td[2]/span/div[1]/a[1]/@rel")
+    #内容提要
     chinf=ress.xpath("//*[@id='oneboolt']//tr/td[3]/text()")
+    
     section_ct=len(href_list)+len(hhr)
     print("可下载章节数："+str(section_ct)+"\r\n")
-    #ti=ress.xpath("//html/body/table[@id='oneboolt']//tr/td[1]/span[1]/h1[1]/span[1]/text()")
     ti=str(titlem[0]).split('_')
     ti=ti[0]+".txt"
     #ti=ids+ti#若文件名不想加编号，可以将这行删除
