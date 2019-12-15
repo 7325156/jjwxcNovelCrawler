fbd1bd12c415fa9cb60e0c7c09598a160ee0e475
7325156 <57527406+7325156@users.noreply.github.com>
2019-12-15 15:07:22 +0800

    以txt格式保存电子书

--- main.py
+++ main.py
@@ -4,42 +4,35 @@ from lxml import etree
 import sys
 import re
 
-
+global aaa
 #小说主地址，后接小说编号
 req_url_base='http://www.jjwxc.net/onebook.php?novelid='
 
 #头文件，可用来登陆，cookie可在浏览器或者client.py中获取
-headers = {
-    'cookie':'',
-    'User-Agent':'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/68.0.3440.106 Safari/537.36'
-}
+headerss={'cookie': 'CNZZDATA30075907=cnzz_eid%3D939945612-1573890182-%26ntime%3D1574667782; testcookie=yes; token=MjAwNzA4OTB8YzkxZmE2Mzg3ODI3ZjU4ZDhhY2ZiZGU1OThlZDBiOWF8fHwxODMxMjQ3fDI1OTIwMDB8MXzliJfosaHlpKnlnqN8fOasoui%2FjuaCqO%2B8jOaZi%2Baxn%2BeUqOaIt3wwfGF1dGhvcm5hbWU%3D; JJSESS=%7B%22referer%22%3A%22/book2/1975581%22%2C%22clicktype%22%3A%22%22%2C%22nicknameAndsign%22%3A%222%257E%2529%2524%25E9%25BB%258E%25E6%2598%258E%22%7D; JJEVER=%7B%22background%22%3A%22%22%2C%22font_size%22%3A%22%22%2C%22isKindle%22%3A%22%22%2C%22ispayuser%22%3A%2220070890-1%22%2C%22foreverreader%22%3A%2220070890%22%2C%22sms_total%22%3A0%7D; UM_distinctid=16eb57cfc5c536-029610602420b1-2393f61-144000-16eb57cfc5d72f; __gads=ID=015f3ae1b0bb2e24:T=1575002897:S=ALNI_MbWjHfBA_i-QcMQOYBYLYSq7ER0Nw', 'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/68.0.3440.106 Safari/537.36'}
 
 #下载单章
-def get_sin(i,headers,fo,chinf):
+def get_sin(i,headers,fo,chinf,aaa,lll):
     tit=i.split('=')
     b=str(tit[2])
     cont=requests.get(i,headers=headers).content
     dot=etree.HTML(cont.decode("GB18030","ignore").encode("utf-8","ignore").decode('utf-8'))
         
-    #正文
+        #获取标题及正文
     tex=dot.xpath("//html/body/table[@id='oneboolt']/tr[2]/td[1]/div[@class='noveltext']/text()")
-    #单章标题
-    he=dot.xpath("string(//html/body/table[@id='oneboolt']/tr[2]/td[1]/div[@class='noveltext']/div[2]/h2)")
-    
-    #获取作话
+    he=dot.xpath("//html/body/table[@id='oneboolt']/tr[2]/td[1]/div[@class='noveltext']/div[2]/h2/text()")
+        #获取作话
     tex1=dot.xpath("//html/body/table[@id='oneboolt']/tr[2]/td[1]/div[@class='noveltext']/div[@class='readsmall']/text()")
-    sign=dot.xpath("//*[@id='oneboolt']/tr[2]/td[1]/div/div[4]/@class") #用以确认正文和作话顺序
-    
-    #写入标题和内容提要
-    tl=str(tit[2])+"#"+" "
-    tll=he[0]
-    if he=="":
+    sign=dot.xpath("//*[@id='oneboolt']/tr[2]/td[1]/div/div[4]/@class")
+    tl=str(tit[2]).zfill(lll)+"#"
+    if he==[]:
         tll="$"
+    else:
+        tll=he[0]
     tl=tl+tll
+        #写入文件
     fo.write(tl.rstrip())
-    fo.write("  "+chinf[int(tit[2])].strip()+"\r\n")
-    
-    #写入正文和作话
+    fo.write(" "+chinf[aaa].strip()+"\r\n")
     if str(sign) == "['readsmall']":
         for m in tex1:
             vv=re.sub('@无限好文，尽在晋江文学城','',str(m))
@@ -69,10 +62,9 @@ def get_sin(i,headers,fo,chinf):
             if v == "\r\n":
                 v=""
             fo.write(v)
-            
-    print("第"+str(tit[2])+"章下载完成")
-
-#下载全部
+    
+    print(tl.rstrip()+" "+chinf[aaa].strip())
+    
 def get_txt(txt_id):
     titlem=''
     intro=''
@@ -85,7 +77,7 @@ def get_txt(txt_id):
     intro=ress.xpath("//html/body/table/tr/td[1]/div[2]/div[@id='novelintro']//text()")
     info=ress.xpath("string(//html/body/table/tr/td[1]/div[3])")
     
-    #获取文章标题
+    #获取标题
     titlem=ress.xpath("//html/head/title/text()")
     print("编号："+ ids + " 小说信息："+ str(titlem[0]) +" 开始下载。\r\n")
 
@@ -95,17 +87,25 @@ def get_txt(txt_id):
     href_list=ress.xpath("//html/body/table[@id='oneboolt']//tr/td[2]/span/div[1]/a/@href")
     #vip
     hhr=ress.xpath("//html/body/table[@id='oneboolt']//tr/td[2]/span/div[1]/a[1]/@rel")
-    #内容提要
     chinf=ress.xpath("//*[@id='oneboolt']//tr/td[3]/text()")
-    
+    for i in chinf:
+        if i.strip()=='':
+            chinf.remove(i)
+    for i in chinf:
+        if i.strip()=='':
+            chinf.remove(i)
+
     section_ct=len(href_list)+len(hhr)
     print("可下载章节数："+str(section_ct)+"\r\n")
+    lll=len(str(section_ct))
+    
     ti=str(titlem[0]).split('_')
     ti=ti[0]+".txt"
-    #ti=ids+ti#若文件名不想加编号，可以将这行删除
     ti=re.sub('/', '_', ti)
     ti=re.sub(r'\\', '_', ti)
-    ti=re.sub('|', '_', ti)
+    ti=re.sub('\|', '_', ti)
+    
+    #ti=ids+ti#若文件名不想加编号，可以将这行删除
     
     v=""
     #打开小说文件写入小说相关信息
@@ -123,13 +123,15 @@ def get_txt(txt_id):
     info=re.sub('\r', '', info)
     info=re.sub('\n', '', info)+"\r\n"
     fo.write(info)
-
+    aaa=0
     #获取每一章内容
     for i in href_list:
-        get_sin(i,headerss,fo,chinf)
+        aaa=aaa+1
+        get_sin(i,headerss,fo,chinf,aaa,lll)
     
     for i in hhr:
-        get_sin(i,headerss,fo,chinf)
+        aaa=aaa+1
+        get_sin(i,headerss,fo,chinf,aaa,lll)
     print("\r\n所有章节下载完成")
     fo.close()
 n=1
