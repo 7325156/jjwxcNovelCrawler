28ee2f7488a416b37f5f1d7ee590f4c940668a74
7325156 <57527406+7325156@users.noreply.github.com>
2019-12-18 17:48:55 +0800

    以epub格式下载保存小说

--- main_epub.py
+++ main_epub.py
@@ -14,7 +14,6 @@ req_url_base='http://www.jjwxc.net/onebook.php?novelid='
 headerss={'cookie': '',
           'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/68.0.3440.106 Safari/537.36'}
 global aaa
-
 #创建epub文件格式信息
 def create_mimetype(epub):
     epub.writestr('mimetype','application/epub+zip',compress_type=zipfile.ZIP_STORED)
@@ -28,15 +27,15 @@ def create_container(epub):
  
 def create_content(epub,path,title,author):
     content_info = '''
-<?xml version="1.0" encoding="utf-8"?>
+<<?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.1//EN"
   "http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd">
 <package version="2.0" unique-identifier="uuid_id" xmlns="http://www.idpf.org/2007/opf">
-  <metadata>
+  <metadata xmlns:opf="http://www.idpf.org/2007/opf" xmlns:dc="http://purl.org/dc/elements/1.1/">
   <dc:title>'''+title+'''</dc:title>
-    <dc:creator>'''+author+'''</dc:creator>
-        </metadata>
-        <manifest>
+  <dc:creator>'''+author+'''</dc:creator>
+</metadata>
+<manifest>
           %(manifest)s <item id="ncx" href="toc.ncx" media-type="text/xml"/>
         <item id="content" href="content.html" media-type="application/xhtml+xml"/>
         <item id="css" href="stylesheet.css" media-type="text/css"/>
@@ -50,14 +49,53 @@ def create_content(epub,path,title,author):
             manifest += '<item id="%s" href="%s" media-type="application/xhtml+xml"/>' % (basename, basename) 
             spine += '<itemref idref="%s"/>' % (basename)
     epub.writestr('OEBPS/content.opf',content_info % {'manifest': manifest,'spine': spine,},compress_type=zipfile.ZIP_STORED)
- 
+
+def create_tox(epub,path,index,rosn):
+    tox_info='''<?xml version="1.0" encoding="UTF-8"?>
+<!DOCTYPE ncx PUBLIC "-//NISO//DTD ncx 2005-1//EN"
+   "http://www.daisy.org/z3986/2005/ncx-2005-1.dtd">
+
+<ncx xmlns="http://www.daisy.org/z3986/2005/ncx/" version="2005-1">
+  <head>
+    <meta name="dtb:uid" content=""/>
+    <meta name="dtb:depth" content="2"/>
+    <meta name="dtb:totalPageCount" content="0"/>
+    <meta name="dtb:maxPageNumber" content="0"/>
+  </head>
+  <docTitle>
+    <text>Unknown</text>
+  </docTitle>
+  <navMap>'''
+    sig=0
+    for html in os.listdir(path):
+        basename = os.path.basename(html)
+        if basename.endswith('html'):
+            if basename!='C.xhtml':
+                if index[sig] in rosn:
+                    if index[sig]==rosn[0]:
+                        tox_info+='''<navPoint id="'''+str(sig)+'''" playOrder="'''+str(sig)+'''">
+                        <navLabel><text>'''+index[sig]+'''</text></navLabel>
+                        <content src="'''+basename+'''"/>'''
+                    else:
+                        tox_info+='''</navPoint><navPoint id="'''+str(sig)+'''" playOrder="'''+str(sig)+'''">
+                        <navLabel><text>'''+index[sig]+'''</text></navLabel>
+                        <content src="'''+basename+'''"/>'''
+                    sig+=1
+                tox_info+='''<navPoint id="'''+str(sig)+'''" playOrder="'''+str(sig)+'''">
+                <navLabel><text>'''+index[sig]+'''</text></navLabel>
+                <content src="'''+basename+'''"/></navPoint>'''
+                sig+=1
+    tox_info+='''</navPoint></navMap></ncx>'''
+    epub.writestr('OEBPS/toc.ncx',tox_info,compress_type=zipfile.ZIP_STORED)
+    
+    
 def create_stylesheet(epub):
     css_info = ''' body {font-family: sans-serif;}'''
     epub.writestr('OEBPS/stylesheet.css',css_info,compress_type=zipfile.ZIP_STORED)
 
-
+global index
 #下载单章
-def get_sin(i,headers,chinf,aaa,lll,rosn,rossn):
+def get_sin(i,headers,chinf,aaa,lll,rosn,rossn,index):
     tit=i.split('=')
     b=str(tit[2])
     cont=requests.get(i,headers=headers).content
@@ -83,6 +121,7 @@ def get_sin(i,headers,chinf,aaa,lll,rosn,rossn):
         tl=tl.rstrip()+" "+chinf[aaa].strip()
         #创建章节文件
         fo=open("z"+str(tit[2].zfill(3))+".xhtml",'w',encoding='utf-8')
+        
         fo.write('''<?xml version="1.0" encoding="utf-8"?>
                 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
                     <html xmlns="http://www.w3.org/1999/xhtml">
@@ -90,9 +129,12 @@ def get_sin(i,headers,chinf,aaa,lll,rosn,rossn):
         #写入卷标
         if i in rossn:
             fo.write("<h1>"+rosn[rossn.index(i)]+"</h1>")
+            print("\r\n"+rosn[rossn.index(i)]+"\r\n")
+            index.append(rosn[rossn.index(i)])
 
         #写入标题
         fo.write("<h2>"+tl+"</h2>")
+        index.append(tl)
 
         #作话在文前的情况
         if str(sign) == "['readsmall']":
@@ -102,7 +144,7 @@ def get_sin(i,headers,chinf,aaa,lll,rosn,rossn):
                 if v!="":#按行写入正文
                     fo.write("<p>"+v+"</p>")
             if len(tex1)!=0:
-                fo.write("<p>----------</p>")
+                fo.write("<hr/>")
             for tn in tex:
                 vv=re.sub('@无限好文，尽在晋江文学城','',str(tn))
                 v=re.sub(' +', ' ', vv).rstrip()
@@ -115,14 +157,14 @@ def get_sin(i,headers,chinf,aaa,lll,rosn,rossn):
                 if v!="":
                     fo.write("<p>"+v+"</p>")
             if len(tex1)!=0:
-                fo.write("<p>----------</p>")
+                fo.write("<hr/>")
             for m in tex1:
                 vv=re.sub('@无限好文，尽在晋江文学城','',str(m))
                 v=re.sub(' +', ' ', vv).rstrip()
                 if v!="":
                     fo.write("<p>"+v+"</p>")
         fo.write("</body></html>")
-        print(tl.rstrip())
+        print("    "+tl.rstrip())
     
 def get_txt(txt_id):
     titlem=''
@@ -160,23 +202,32 @@ def get_txt(txt_id):
     href_list=ress.xpath("//html/body/table[@id='oneboolt']//tr/td[2]/span/div[1]/a/@href")
     #vip
     hhr=ress.xpath("//html/body/table[@id='oneboolt']//tr/td[2]/span/div[1]/a[1]/@rel")
+    
     #每章内容提要
+    loc=ress.xpath("//*[@id='oneboolt']//tr/td[2]/span/div[1]/span/ancestor-or-self::tr/td[3]/text()")
     chinf=ress.xpath("//*[@id='oneboolt']//tr/td[3]/text()")
+    
     for i in chinf:
-        if i.strip()=='':
-            chinf.remove(i)
+        if i.strip()=='[此章节已锁]':
+            del chinf[chinf.index(i)]
     for i in chinf:
-        if i.strip()=='':
-            chinf.remove(i)
-                    
+        if i.strip()=='[此章节已锁]':
+            del chinf[chinf.index(i)]
+    for i in chinf:
+        if i in loc:
+            del chinf[chinf.index(i)]
+    for i in chinf:
+        if i in loc:
+            del chinf[chinf.index(i)]
+
     #获取卷标名称
     rosn=ress.xpath("//*[@id='oneboolt']//tr/td/b[@class='volumnfont']/text()")
     #获取卷标位置
     rossn=ress.xpath("//*[@id='oneboolt']//tr/td/b/ancestor-or-self::tr/following-sibling::tr[1]/td[2]/span/div[1]/a[1]/@href")
-    rossn.append(ress.xpath("//*[@id='oneboolt']//tr/td/b/ancestor-or-self::tr/following-sibling::tr[1]/td[2]/span/div[1]/a[1]/@rel"))
+    rossn+=ress.xpath("//*[@id='oneboolt']//tr/td/b/ancestor-or-self::tr/following-sibling::tr[1]/td[2]/span/div[1]/a[1]/@rel")
 
-    
     section_ct=len(href_list)+len(hhr)
+
     print("可下载章节数："+str(section_ct)+"\r\n")
     #获取填充序号的长度，例如：若全文有1437章，则每章序号有四位，依次为0001、0002……
     lll=len(str(section_ct))
@@ -209,6 +260,7 @@ def get_txt(txt_id):
     pic.write(img)
     pic.close()
 
+    index=[]
     #写入封面
     f=open("C.xhtml",'w',encoding='utf-8')
     f.write('''<?xml version="1.0" encoding="utf-8"?>
@@ -222,8 +274,9 @@ def get_txt(txt_id):
              <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
                     <html xmlns="http://www.w3.org/1999/xhtml"><head><title>'''+ti+'''</title></head><body>''')
 
-    for nn in titlem:
-        fo.write("<h2><a href='"+req_url+"'>"+str(nn)+"</a></h2><p></p>")
+
+    fo.write("<h2><a href='"+req_url+"'>"+titlem[0]+"</a></h2><p></p>")
+    index.append(titlem[0])
 
     for ix in infox:
         ix=ix.strip()
@@ -236,18 +289,18 @@ def get_txt(txt_id):
         if v!="":
             fo.write("<p>"+v+"</p>")
     info=re.sub(' +', ' ',info).strip()
-    info=re.sub('搜索关键字','\r\n搜索关键字',info)
-    fo.write(info)
+    info=re.sub('搜索关键字','</p><p>搜索关键字',info)
+    fo.write("<p>"+info+"</p>")
     fo.write("</body></html>")
     aaa=0
     #获取每一章内容
     for i in href_list:
         aaa=aaa+1
-        get_sin(i,headerss,chinf,aaa,lll)
+        get_sin(i,headerss,chinf,aaa,lll,rosn,rossn,index)
     
     for i in hhr:
         aaa=aaa+1
-        get_sin(i,headerss,chinf,aaa,lll)
+        get_sin(i,headerss,chinf,aaa,lll,rosn,rossn,index)
     fo.close()
     #保存为epub
     os.chdir(path)
@@ -258,6 +311,7 @@ def get_txt(txt_id):
     os.chdir(ti)
     ppp=os.getcwd()
     create_content(epub,ppp,xtitle,xaut)
+    create_tox(epub,ppp,index,rosn)
     create_stylesheet(epub)
     for html in os.listdir('.'):
         basename = os.path.basename(html)
