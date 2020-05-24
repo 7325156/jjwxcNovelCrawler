7606aaa057a1f6452ea6d3b71fbc18e2fbdae833
7325156 <57527406+7325156@users.noreply.github.com>
2020-05-24 18:03:40 +0800

    Update EPUB3.py

--- epub下载/EPUB3.py
+++ epub下载/EPUB3.py
@@ -7,42 +7,53 @@ import zipfile
 import shutil
 class epubfile():
     #创建epub文件格式信息
-    def create_mimetype(epub):
+    author=''
+    title=''
+    description=''
+    TOC=''
+    path=''
+    def create_mimetype(self,epub):
         epub.writestr('mimetype','application/epub+zip',compress_type=zipfile.ZIP_STORED)
      
-    def create_container(epub):
+    def create_container(self,epub):
         container_info = '''<?xml version="1.0" encoding="UTF-8"?>
 <container version="1.0" xmlns="urn:oasis:names:tc:opendocument:xmlns:container">
 <rootfiles><rootfile full-path="OEBPS/content.opf" media-type="application/oebps-package+xml"/>
 </rootfiles></container>'''
         epub.writestr('META-INF/container.xml',container_info, compress_type=zipfile.ZIP_STORED)
      
-    def create_content(epub,path,title,author):
+    def create_content(self,epub,path):
         content_info = '''<?xml version="1.0" encoding="utf-8"?>
 <package version="3.0" unique-identifier="BookId" xmlns="http://www.idpf.org/2007/opf">
-  <metadata xmlns:dc="http://purl.org/dc/elements/1.1/">
-<dc:title>'''+title+'''</dc:title>
-<dc:creator>'''+author+'''</dc:creator>
-<meta name="cover" content="p.jpg" />
+  <metadata xmlns:dc="http://purl.org/dc/elements/1.1/">'''
+        if self.title!='':
+            content_info+="<dc:title>"+self.title+"</dc:title>"
+        if self.author!='':
+            content_info+="<dc:creator>"+self.author+"</dc:creator>"
+        if self.description!='':
+            content_info+="<dc:description>"+self.description+"</dc:description>"
+        content_info+='''<meta name="cover" content="p.jpg" />
 </metadata><manifest>
 <item id="sgc-nav.css" href="sgc-nav.css" media-type="text/css"/>
-<item id="nav.xhtml" href="nav.xhtml" media-type="application/xhtml+xml" properties="nav"/>
+<item id="TOC.xhtml" href="TOC.xhtml" media-type="application/xhtml+xml" properties="nav"/>
 <item id="p.jpg" href="p.jpg" media-type="image/jpeg" properties="cover-image"/>
 %(manifest)s
 </manifest><spine>
+<itemref idref="C.xhtml" />
+<itemref idref="TOC.xhtml" />
 %(spine)s
-<itemref idref="nav.xhtml" linear="no"/>
 </spine></package>'''
         manifest = ''
         spine = ''
         for html in os.listdir(path):
             basename = os.path.basename(html)
             if basename.endswith('html'):
-                manifest += '<item id="%s" href="%s" media-type="application/xhtml+xml"/>' % (basename, basename) 
-                spine += '<itemref idref="%s"/>' % (basename)
+                manifest += '<item id="%s" href="%s" media-type="application/xhtml+xml"/>' % (basename, basename)
+                if basename != 'C.xhtml':
+                    spine += '<itemref idref="%s"/>' % (basename)
         epub.writestr('OEBPS/content.opf',content_info % {'manifest': manifest,'spine': spine,},compress_type=zipfile.ZIP_STORED)
 
-    def create_info(epub,path,index,rollSign,title):
+    def create_info(self,epub,path,index,rollSign):
         nav_info='''<?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE html>
 
@@ -51,12 +62,12 @@ class epubfile():
 <meta charset="utf-8"/>
 <link href="sgc-nav.css" rel="stylesheet" type="text/css"/>
 </head>
-<body epub:type="frontmatter">
-  <nav epub:type="toc" id="toc"><h1>目录</h1>
+<body>'''+self.TOC+'''
+<nav epub:type="toc" id="toc"><h1>目录</h1>
     <ol>
     '''
         sig=0
-        nav_info+='''<li><a href="TOC.xhtml">'''+title+'''</a>
+        nav_info+='''<li><a href="TOC.xhtml">'''+self.title+'-'+self.author+'''</a>
 <ol>'''
         for html in os.listdir(path):
             basename = os.path.basename(html)
@@ -76,10 +87,10 @@ class epubfile():
 '''
                     sig+=1
         nav_info+='''</ol></li></ol></nav></body></html>'''
-        epub.writestr('OEBPS/nav.xhtml',nav_info,compress_type=zipfile.ZIP_STORED)
+        epub.writestr('OEBPS/TOC.xhtml',nav_info,compress_type=zipfile.ZIP_STORED)
  
         
-    def create_stylesheet(epub):
+    def create_stylesheet(self,epub):
         css_info = '''nav#landmarks {
     display:none;
 }
