2944bc553cfd52f934fbda3c265bb8d3bc3c1e17
7325156 <57527406+7325156@users.noreply.github.com>
2020-09-21 09:57:52 +0800

    Update main_epub.py

--- epub下载/main_epub.py
+++ epub下载/main_epub.py
@@ -8,14 +8,15 @@ import zipfile
 import shutil
 from opencc import OpenCC
 import concurrent.futures
-import EPUB3
+#若更改epub格式，可将所有EPUB2修改为EPUB3
+import EPUB2
 
 class noveldl():
     #小说主地址，后接小说编号
     req_url_base='http://www.jjwxc.net/onebook.php?novelid='
 
-    #头文件，可用来下载账号中已购买文章，cookie可在浏览器或者client.py中获取
-    headerss={'cookie':'',
+    #头文件，可用来登陆，cookie可在浏览器或者client.py中获取
+    headerss={'cookie': '',
               'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3904.108 Safari/537.36'}
 
     percent=0
@@ -28,6 +29,8 @@ class noveldl():
     state=''#繁简转换状态
     href_list=[]#章节链接
     td=[]
+    failInfo=[]
+    titleInfo=[1,1,1]
     
     def clear(self):
         self.percent=0
@@ -60,13 +63,16 @@ class noveldl():
 
         title=''
         #序号填充
-        title=str(titleOrigin[2]).zfill(self.fillNum)+" "
+        if self.titleInfo[0]=='1':
+            title=str(titleOrigin[2]).zfill(self.fillNum)
         
         #章节名称
-        title=title+self.titleindex[i].strip()
+        if self.titleInfo[1]=='1':
+            title=title+" "+self.titleindex[i].strip()
         
         #内容提要
-        title=title+" "+self.Summary[i].strip()
+        if self.titleInfo[2]=='1':
+            title=title+" "+self.Summary[i].strip()
         
         if self.state=='s':
             title=OpenCC('t2s').convert(title)
@@ -82,7 +88,8 @@ class noveldl():
             
             
         if len(he)==0:
-            print("第"+titleOrigin[2]+"章未购买或加载失败")
+            self.failInfo.append(titleOrigin[2].zfill(self.fillNum))
+            #print("第"+titleOrigin[2]+"章未购买或加载失败")
         else:
             #创建章节文件
             fo=open("z"+str(titleOrigin[2].zfill(4))+".xhtml",'w',encoding='utf-8')
@@ -98,14 +105,14 @@ class noveldl():
             if self.href_list[i] in self.rollSignPlace:
                 fo.write("<h1>"+v.rstrip()+"</h1>")
                 print("\r\n"+v+"\r\n")
-                fo.write("<h2 id='v'>"+title+"</h2>")
+                fo.write("<h2 id='v'><a href='"+v+"'>"+title+"</a></h2>")
             #写入标题
             else:
                 v=re.sub('&','&amp;',l)
                 fo.write('<h2><a href="'+v+'">'+title+"</a></h2>")
             #作话在文前的情况
             if str(sign) == "['readsmall']":
-                fo.write('''<blockquote class="userstuff">''')
+                fo.write('''<blockquote>''')
                 for m in tex1:#删除无用文字及多余空格空行
                     vv=re.sub('@无限好文，尽在晋江文学城','',str(m))
                     v=re.sub(' +', ' ', vv).strip()
@@ -116,6 +123,7 @@ class noveldl():
                         v=OpenCC('t2s').convert(v)
                     elif self.state=='t':
                         v=OpenCC('s2t').convert(v)
+                    v=re.sub('作者有话要说：','<b>作者有话要说</b>：</p><p>',v)
                     if v!="":#按行写入正文
                         fo.write("<p>"+v+"</p>")
                 fo.write("</blockquote>")
@@ -149,7 +157,7 @@ class noveldl():
                         fo.write("<p>"+v+"</p>")
                 if len(tex1)!=0:
                     fo.write("<hr/>")
-                    fo.write('''<blockquote class="userstuff">''')
+                    fo.write('''<blockquote>''')
                 for m in tex1:
                     vv=re.sub('@无限好文，尽在晋江文学城','',str(m))
                     v=re.sub(' +', ' ', vv).strip()
@@ -160,6 +168,7 @@ class noveldl():
                         v=OpenCC('t2s').convert(v)
                     elif self.state=='t':
                         v=OpenCC('s2t').convert(v)
+                    v=re.sub('作者有话要说：','<b>作者有话要说</b>：</p><p>',v)
                     if v!="":
                         fo.write("<p>"+v+"</p>")
                 if len(tex1)!=0:
@@ -321,7 +330,7 @@ class noveldl():
         #写入文章信息页 
         TOC="<h1 class='title'><a href='"+req_url+"'>"+xtitle+"</a></h1>"
         TOC+="<h2 class='sigil_not_in_toc title'>作者：<a href='"+xauthref+"'>"+xaut+"</a></h2>"
-        TOC+='''<blockquote class="userstuff">'''
+        TOC+='''<blockquote>'''
         #self.index.append(titlem[0])
         #生成目录文字
         for l in self.href_list:
@@ -380,6 +389,13 @@ class noveldl():
         info=re.sub('一句话简介：','</p><p>一句话简介：',info)
         info=re.sub('立意：','</p><p>立意：',info)
         TOC+="<p>"+info+"</p>"
+        fo=open("TOC.xhtml",'w',encoding='utf-8')
+        fo.write('''<?xml version="1.0" encoding="utf-8"?><!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/xhtml" xmlns:epub="http://www.idpf.org/2007/ops">
+<head><title></title><meta charset="utf-8"/>
+<link href="sgc-nav.css" rel="stylesheet" type="text/css"/></head>
+<body>'''+TOC+'''</body></html>''')
+        fo.close()
         tlist=[]
         #获取每一章内容
         with concurrent.futures.ThreadPoolExecutor(max_workers=threadnum) as executor:
@@ -388,14 +404,19 @@ class noveldl():
                 if self.percent < section_ct:
                     print('\r 下载进度：%d/%d' % (self.percent,section_ct),end='',flush=True)
             print('\r 下载完成，总进度：%d/%d\r\n' % (self.percent,section_ct),end='',flush=True)
-
+        if self.failInfo != []:
+            self.failInfo.sort()
+            vs=""
+            for ss in self.failInfo:
+                vs=vs+ss+"|"
+            print("\r\n未购买或加载失败章节：")
+            print(vs[:-1]+"\r\n")
 
         #保存为epub
         os.chdir(path)
         epub_name = ti+".epub"
         epub = zipfile.ZipFile(epub_name, 'w')
-        epubfile=EPUB3.epubfile()
-        epubfile.TOC=TOC
+        epubfile=EPUB2.epubfile()
         epubfile.author=xaut
         epubfile.title=xtitle
         epubfile.create_mimetype(epub)     
@@ -415,10 +436,26 @@ class noveldl():
         os.chdir(path)
         shutil.rmtree(ppp)
         print("\r\nepub打包完成")
+
+if __name__ == '__main__':
+    #print('请输入cookie：')
+    #cookie=input()
+    #此处为需要下载小说的编号，编号获取方法在上文中已经讲过，
+    while 1:
+        num =input('\r\n请输入小说主页网址：')
+        c=noveldl()
         
-while 1:
-    num =input('\r\n请输入小说主页网址：')
-    c=noveldl()
-    state=input('\r\n文章内容：\r\n1、繁转简（输入s）\r\n2、简转繁（输入t）\r\n3、不变（直接按回车）\r\n')
-    c.get_txt(num,state,10)#最后一个参数为线程数，可以对其进行更改
-    c.clear()
+        #c.headerss={'cookie':cookie,
+        #          'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3904.108 Safari/537.36'}
+
+        state=input('\r\n文章内容：\r\n1、繁转简（输入s）\r\n2、简转繁（输入t）\r\n3、不变（直接按回车）\r\n')
+        
+        titleInfo=input('\r\n请输入标题保存状态(序号 章节名称 内容提要)\r\n显示则输入1，不显示则输入0，数字之间用空格隔开\r\n例如：若只显示序号和内容提要，则输入[1 0 1](方括号不输入)\r\n若全部显示，可以直接按回车，若不显示标题，可以直接输入0\r\n若输入的数字个数小于3，则空缺的数字与最后输入的数字相同\r\n')
+        if titleInfo=='':
+            titleInfo='1 1 1'
+        titleInfo=titleInfo.split(' ')
+        while len(titleInfo)<3:
+            titleInfo.append(titleInfo[len(titleInfo)-1])
+        c.titleInfo=titleInfo
+        
+        c.get_txt(num,state,50)
