ff0b87aa07619c8b54b7bd23a314d708102e08b4
7325156 <57527406+7325156@users.noreply.github.com>
2021-02-05 21:36:24 +0800

    未完成对照表

---
+++ 反爬虫对照表/字体处理.py
@@ -0,0 +1,40 @@
+from fontTools.ttLib import TTFont
+import os
+import re
+import concurrent.futures
+
+global nnn
+nnn=''
+def convertXML(name):
+    font = TTFont(name+'.woff2')
+    font.saveXML(name+'.xml')
+    font.close()
+def createLIST(name):
+    tlist=[]
+    print(name)
+    with open(name+'.xml','r') as f:
+        c=f.readlines()
+        for l in c:
+            y=re.search(r'GlyphID.*?uni....',l)
+            if not y==None:
+                y=re.sub(r'GlyphID id.*?name..uni','&#x',y.group())
+                y=y+';-'
+                tlist.append(y)
+    fo=open(name+'.empty.txt','w',encoding='utf-8')
+    for y in tlist:
+        fo.write(y+'\n')
+    fo.close()
+def conf(name):
+    if name.endswith('woff2'):
+        name=re.sub('.woff2','',name)
+        nnn=name
+        if not os.path.exists(name+'.xml'):
+            convertXML(name)
+        if not os.path.exists(name+'.empty.txt'):
+            createLIST(name)
+
+if __name__ == '__main__':
+    list=os.listdir()
+    with concurrent.futures.ThreadPoolExecutor(max_workers=20) as executor:
+        tlist = {executor.submit(conf,i):i for i in list}
+        concurrent.futures.as_completed(tlist)
